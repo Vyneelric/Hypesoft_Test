2ba53b5d0069fd53682c1beccdd2e178704deab7
Vyneelric <vini.cardoso1909@gmail.com>
2026-03-26 23:45:56 -0300

    fix(dashboard): fix table and chart section layout

--- Front-End_Hypesoft/src/pages/Dashboard.tsx
+++ Front-End_Hypesoft/src/pages/Dashboard.tsx
@@ -50,7 +50,7 @@ export function Dashboard() {
           </Card>
         </div>
 
-        <div className="w-full h-auto mt-6 lg:mt-10 flex-col gap-6 lg:gap-10">
+        <div className="w-full h-auto mt-6 lg:mt-10 flex flex-col lg:flex-row gap-6 lg:gap-10 items-start">
           <Card className="border-none ring-0 w-full lg:w-3/5 bg-[#FFFFFF]">
             <CardHeader className="flex flex-row items-center gap-2">
               <Scroll className="!w-8 !h-8 sm:!w-9 sm:!h-9 bg-[#F9F9F9] rounded-xl p-1" stroke="#4E36DF" strokeWidth={1.6} />
